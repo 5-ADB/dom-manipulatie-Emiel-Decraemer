4f1915e8064caed59aa5bd246c1d2e35d1d10ba1
emieldecraemer <emiel.decraemer@student.tsaam.be>
2025-03-18 11:57:30 +0100

    eerste oush

--- script/script.js
+++ script/script.js
@@ -5,3 +5,39 @@ const gebruikers = [
   { id: 4, login: "jos.teugels", leeftijd: 13 },
   { id: 5, login: "piet.pollekes", leeftijd: 27 },
 ];
+
+const gevraagdeLeeftijd = document.querySelector("#filterLeeftijd")
+const lijstnamen = document.querySelector("#lijstNamen")
+const jonger = document.querySelector("#jonger")
+const ouder = document.querySelector("#ouder")
+const toevoegen = document.querySelector("#toevoegen")
+
+//een naam en leeftijd toevoegen aan een object
+
+
+
+// kijken of een naaam ouder of jonger is
+jonger.addEventListener("click", kijkJonger())
+
+
+ouder.addEventListener("click", kijkOuder)
+
+function kijkJonger(gebruikers) {
+    gebruikers.forEach(leeftijd => {
+  if (gevraagdeLeeftijd.value >= leeftijd){
+    const ouderOfJongerNamen = document.createElement("p")
+    ouderOfJongerNamen.textContent = login
+    lijstnamen.appendChild(ouderOfJongerNamen)
+  }
+ })
+}
+
+function kijkOuder(gebruikers) {
+  gebruikers.forEach(leeftijd => {
+  if (gevraagdeLeeftijd.value <= leeftijd){
+    const ouderOfJongerNamen = document.createElement("p")
+    ouderOfJongerNamen.textContent = login
+    lijstnamen.appendChild(ouderOfJongerNamen)
+  }
+ })
+}
